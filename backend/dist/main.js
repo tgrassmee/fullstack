aad45f263b7f332c048908e2f802b92eb41a48dc
tombo <tomboy0815@web.de>
2021-12-07 12:38:27 +0100

    add Axios call RestCountries Api and prompt it to console

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -1,2 +1,13 @@
 "use strict";
-const a = "das ist TS neu";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+axios_1.default.get("https://restcountries.com/v3.1/all")
+    .then((x) => {
+    x.data.forEach((s) => {
+        console.log(s.name.official);
+    });
+})
+    .catch((error) => { console.log(error); });
--- backend/main.ts
+++ backend/main.ts
@@ -1,1 +1,13 @@
-const a="das ist TS neu";
+import axios from 'axios'
+
+
+axios.get("https://restcountries.com/v3.1/all")
+    .then((x)=>
+    {
+        x.data.forEach((s:any)=>{
+            console.log(s.name.official)
+        })
+
+    })
+    .catch((error)=>{console.log(error)})
+
